da89ce86a37d326f1218df2d9e04bb0f34b88364
VictorManuelCH <victor.castaneda@unas.edu.pe>
2025-03-17 05:05:22 +0000

    SELENIUM PRUEBA 4 CI/CD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_18_211334) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_17_050338) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -200,7 +200,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_18_211334) do
     t.string "email"
     t.string "username"
     t.string "encrypted_password"
-    t.bigint "role_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "reset_password_token"
@@ -208,7 +207,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_18_211334) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
@@ -232,5 +230,4 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_18_211334) do
   add_foreign_key "user_addresses", "users"
   add_foreign_key "user_roles", "roles"
   add_foreign_key "user_roles", "users"
-  add_foreign_key "users", "roles"
 end
