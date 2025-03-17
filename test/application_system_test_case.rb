bb320ea83ed9d21f5f372335ddd69818a583ef54
VictorManuelCH <victor.castaneda@unas.edu.pe>
2025-03-17 05:33:37 +0000

    SELENIUM PRUEBA 13 CI/CD

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,10 +3,17 @@ require "capybara/rails"
 require "selenium/webdriver"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  options = Selenium::WebDriver::Chrome::Options.new
+
   if ENV["GITHUB_ACTIONS"]
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1400,1400")
+
+    driven_by :selenium, using: :chrome, options: { browser: :chrome, options: options }
   else
     driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   end
-
 end
