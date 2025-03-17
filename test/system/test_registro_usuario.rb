62972cfcd76aafa21ba4e8cdcd245eccec944c43
VictorManuelCH <victor.castaneda@unas.edu.pe>
2025-03-17 05:21:44 +0000

    SELENIUM PRUEBA 8 CI/CD

---
+++ test/system/test_registro_usuario.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class UsersTest < ActionDispatch::SystemTestCase
+  setup do
+    @role = Role.create!(name: "Customer")
+    @user = User.create!(
+      email: "user@example.com",
+      username: "user1",
+      encrypted_password: "password1",
+      role: @role
+    )
+  end
+
+  test "un usuario puede registrarse" do
+    visit new_user_registration_path
+    fill_in "Email", with: @user.email
+    fill_in "Contraseña", with: "password1"
+    fill_in "Confirmación de contraseña", with: "password1"
+    click_button "Registrarse"
+
+    assert_text "Bienvenido"
+  end
+end
