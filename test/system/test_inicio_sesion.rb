77b57587eaf40f18b7d60ecc20effd78f7b4c515
VictorManuelCH <victor.castaneda@unas.edu.pe>
2025-03-17 15:03:32 +0000

    SELENIUM PRUEBA 51 CI/CD

--- test/system/test_inicio_sesion.rb
+++ test/system/test_inicio_sesion.rb
@@ -4,23 +4,26 @@ require "application_system_test_case"
 class LoginTest < ApplicationSystemTestCase
   setup do
     User.destroy_all
+    Role.destroy_all # Elimina roles previos para evitar duplicados
     @role = Role.create!(name: "Cliente")
 
+    # Crear un usuario con el rol asociado
     @user = User.create!(
-      email: "test_user@example.com",
-      password: "password123",
-      password_confirmation: "password123",
-      role: @role
+      email: "test@example.com",
+      password: "password1",
+      password_confirmation: "password1",
+      role: @role # Asegurar que el usuario tenga un rol válido
     )
   end
 
   test "un usuario puede iniciar sesión" do
-    visit new_user_session_path
+    visit new_user_session_path # Ir a la página de inicio de sesión
 
-    fill_in "Correo electrónico", with: @user.email  
-    fill_in "Contraseña", with: "password123"
-    click_button "Iniciar sesión"  
+    fill_in "Correo Electrónico", with: @user.email
+    fill_in "Contraseña", with: "password1"
+    click_button "Iniciar Sesión"
 
     assert_text "BIENVENIDO A NUESTRA TIENDA"
   end
 end
+
