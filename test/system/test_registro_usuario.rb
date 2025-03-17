b5ea9f3bc264d4e72eb8054d51109e8f341f1832
VictorManuelCH <victor.castaneda@unas.edu.pe>
2025-03-17 06:49:06 +0000

    SELENIUM PRUEBA 25 CI/CD

--- test/system/test_registro_usuario.rb
+++ test/system/test_registro_usuario.rb
@@ -2,24 +2,25 @@ require "test_helper"
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  setup do
-    @role = Role.create!(name: "Customer")
-    @user = User.create!(
-      email: "user@example.com",
-      username: "user1",
-      password: "password1",
-      password_confirmation: "password1"
-    )
-    UserRole.create!(user: @user, role: @role)
-  end
+    setup do
+        @role = Role.create!(name: "Customer")
+        @user = User.create!(
+          email: "user@example.com",
+          username: "user1",
+          password: "password1",
+          password_confirmation: "password1",
+          role_id: @role.id # <- Agrega esto
+        )
+    end
+      
 
-  test "un usuario puede registrarse" do
-    visit new_user_registration_path
-    fill_in "Email", with: @user.email
-    fill_in "Contraseña", with: "password1"
-    fill_in "Confirmación de contraseña", with: "password1"
-    click_button "Registrarse"
+    test "un usuario puede registrarse" do
+        visit new_user_registration_path
+        fill_in "Email", with: @user.email
+        fill_in "Contraseña", with: "password1"
+        fill_in "Confirmación de contraseña", with: "password1"
+        click_button "Registrarse"
 
-    assert_text "Bienvenido"
-  end
+        assert_text "Bienvenido"
+    end
 end
