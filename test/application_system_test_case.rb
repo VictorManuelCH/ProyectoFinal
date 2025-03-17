7e45de8043ff5156815c10b7d01bba847f6500a0
VictorManuelCH <victor.castaneda@unas.edu.pe>
2025-03-17 05:47:27 +0000

    SELENIUM PRUEBA 16 CI/CD

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,26 +5,19 @@ require "selenium/webdriver"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   options = Selenium::WebDriver::Chrome::Options.new
 
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--window-size=1400,1400")
+  options.add_argument("--disable-features=NetworkService,NetworkServiceInProcess")
+  
+  # Establecer un directorio único para evitar el error "user data directory is already in use"
+  options.add_argument("--user-data-dir=/tmp/chrome-user-data")
+
   if ENV["GITHUB_ACTIONS"]
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1400,1400")
-
-    driven_by :selenium, using: :chrome, options: { browser: :chrome, options: options }
-  else
-    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+    options.add_argument("--headless") # Solo en GitHub Actions para ejecutar en modo headless
   end
 
-  Capybara.register_driver :selenium do |app|
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--headless') # Asegurar que se ejecute en modo headless en CI
-    options.add_argument("--user-data-dir=/tmp/chrome-user-data") # Directorio único en CI
-  
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-  end
-  
+  driven_by :selenium, using: :chrome, options: { browser: :chrome, options: options }
 end
+
