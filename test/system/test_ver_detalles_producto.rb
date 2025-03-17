27541004a5583dba985dc6dc99b87c98af974a2f
VictorManuelCH <victor.castaneda@unas.edu.pe>
2025-03-17 14:23:21 +0000

    SELENIUM PRUEBA 46 CI/CD

--- test/system/test_ver_detalles_producto.rb
+++ test/system/test_ver_detalles_producto.rb
@@ -2,19 +2,21 @@ require "test_helper"
 require "application_system_test_case"
 
 class ProductDetailsTest < ApplicationSystemTestCase
-  # 🛒 Crear una categoría si es necesaria
-  categoria = Category.create!(name: "Electrónica")
-
-  # 📦 Crear un producto con categoría
-  producto = Product.create!(
-    name: "Laptop Gamer",
-    description: "Una laptop potente para gaming",
-    price: 1500.00,
-    quantity: 10,
-    categories: [categoria]  # Asegura que tenga categorías asociadas
-  )
+  
 
   test "un usuario puede ver los detalles de un producto" do
+    # 🛒 Crear una categoría si es necesaria
+    categoria = Category.create!(name: "Electrónica")
+
+    # 📦 Crear un producto con categoría
+    producto = Product.create!(
+        name: "Laptop Gamer",
+        description: "Una laptop potente para gaming",
+        price: 1500.00,
+        quantity: 10,
+        categories: [categoria]  # Asegura que tenga categorías asociadas
+    )
+    
     visit products_path
 
     assert_text "Producto de prueba"
